de10aef19324e1349a064de73d995d78d41e32c4
Vinícius <147394792+vinizinhosuculento@users.noreply.github.com>
2025-07-02 11:13:30 -0300

    a

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ const perguntas = [
         ]
     }
 ];
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
